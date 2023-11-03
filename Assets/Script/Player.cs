1265f989bbd31b31f1c7ace24e14c8e008e22bbe
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-11-03 15:45:09 +0900

    ジャンプ処理追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,18 +5,22 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // 変数の意味を視覚的にわかるようにする
+    // ヘッダーの設定
     [SerializeField, Header("移動速度")]
     private float _moveSpeed;
+    [SerializeField, Header("ジャンプ速度")]
+    private float _jumpSpeed;
 
     private Vector2 _inputDirection;
     private Rigidbody2D _rigid;
+    private bool _bJump;
 
     // Start is called before the first frame update
     void Start()
     {
         // Rigidbody2Dのコンポーネント取得
         _rigid = GetComponent<Rigidbody2D>();
+        _bJump = false;
     }
 
     // Update is called once per frame
@@ -33,9 +37,29 @@ public class Player : MonoBehaviour
     }
 
     // 移動した入力情報を代入する
-    public void _OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
         // vector2型に変換
         _inputDirection = context.ReadValue<Vector2>();
+    }
+
+    // 当たり判定
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            _bJump = false;
+        }
+    }
+
+    // Jumpアクション
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // jumpキーではない場合
+        if (!context.performed || _bJump) return;
+
+        // 上方向に力を加える
+        _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);  // ForceMode2D.Impulseは初速が早く、徐々に減速させる
+        _bJump = true;
     }
 }
